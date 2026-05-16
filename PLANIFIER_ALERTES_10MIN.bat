ff2c0a53dcc2912bc5de67688d307a68588d8045
Verda7 <david.vergucht@gmail.com>
2026-05-16 08:23:58 +0200

    Delete PLANIFIER_ALERTES_10MIN.bat

--- PLANIFIER_ALERTES_10MIN.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "%~dp0"
-schtasks /Create /TN "StationMeteoAlertesTelegram" /TR "\"%CD%\VERIFIER_ALERTES.bat\"" /SC MINUTE /MO 10 /F
-echo.
-echo Tache planifiee creee : verification alertes toutes les 10 minutes.
-pause
